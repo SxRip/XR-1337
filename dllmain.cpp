2c0647609667edfa937dea3edd1249634ff47357
Sora <68508786+SxRip@users.noreply.github.com>
2022-10-08 22:31:50 +0200

    Inject successful

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,28 +1,29 @@
-﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
-#include "offsets/offsets.h"
-#include "..\\xrEngine-1337\include\ConsoleMessages.h"
+﻿#include "offsets/offsets.h"
+#include "..\\xrEngine-1337\include\handles.hpp"
 
-void func()
+void WINAPI Main(HMODULE hModule)
 {
-	MessageBox(nullptr, "Hello", nullptr, 0);
+	
+	FreeLibraryAndExitThread(hModule, 0);
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-	DWORD  ul_reason_for_call,
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call,
 	LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	{
-		HANDLE hThread = CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(func),
-			nullptr, 0, nullptr);
+		case DLL_PROCESS_ATTACH:
+		{
+			HANDLE hThread = CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(Main),
+				hModule, 0, nullptr);
 
-	}
-	case DLL_PROCESS_DETACH:
-		break;
+			if (!hThread)
+			{
+				MessageBox(nullptr, "Can't create the thread", nullptr, MB_ICONERROR);
+				return -1;
+			}
+			SafeCloseHandle(hThread);
+		}
 	}
 
 	return TRUE;
