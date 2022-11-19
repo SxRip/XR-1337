8979f52ce9690dcd5b1b13a5c1fd4e8e4ef1c8b2
Sora <68508786+SxRip@users.noreply.github.com>
2022-11-19 21:42:55 +0100

    changed main function. Preparing for release

--- dllmain.cpp
+++ dllmain.cpp
@@ -5,37 +5,6 @@ void WINAPI Main(HMODULE hModule)
 {
 	Memory mem;
 
-	while (!GetAsyncKeyState(VK_END))
-	{
-		_Offset_Ptr<float> pStamina = mem.get_pointer<float>(offsets::initialized::actor::stamina);
-		_Offset_Ptr<float> pHP = mem.get_pointer<float>(offsets::initialized::actor::HP);
-		_Offset_Ptr<float> pWeight = mem.get_pointer<float>(offsets::initialized::actor::weight);
-		_Offset_Ptr<float> pCrosshairDelayInfo = mem.get_pointer<float>(offsets::initialized::actor::crosshairDelayInfo);
-		_Offset_Ptr<float> pFov = mem.get_pointer<float>(offsets::initialized::actor::fov);
-
-		_Offset_Ptr<float> pX = mem.get_pointer<float>(offsets::initialized::actor::coordX);
-		_Offset_Ptr<float> pY = mem.get_pointer<float>(offsets::initialized::actor::coordY);
-		_Offset_Ptr<float> pZ = mem.get_pointer<float>(offsets::initialized::actor::coordZ);
-
-		_Offset_Ptr<const char> pActorName = mem.get_pointer<const char>(offsets::initialized::actor::name);
-
-		_Offset_Ptr<size_t> pActorFireState = mem.get_pointer<size_t>(offsets::initialized::actor::fireState);
-		_Offset_Ptr<size_t> pTargetType = mem.get_pointer<size_t>(offsets::initialized::actor::targetType);
-
-		_Offset_Ptr<int> pMoney = mem.get_pointer<int>(offsets::initialized::actor::money);
-
-		_Offset_Ptr<bool> pWeaponInHands = mem.get_pointer<bool>(offsets::initialized::actor::weaponIsInHands);
-
-		pStamina = 1;
-
-		pWeight = 0;
-
-		if (*pMoney < 1000000)
-			pMoney += 100000;
-
-		pCrosshairDelayInfo = 1;
-	}
-
 	FreeLibraryAndExitThread(hModule, 0);
 }
 
