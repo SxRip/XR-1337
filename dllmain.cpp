89abf4af3b9662bd873f396fbd0cfb2f30962949
Sora <68508786+SxRip@users.noreply.github.com>
2022-11-21 21:33:16 +0100

    merged with master but offsets are initialized in a main thread

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,9 +1,63 @@
 ﻿#define STALKERNET
+
 #include "XR-1337/includes/XR-1337.hpp"
+#include "1337 loader/include/processes.hpp"
 
 void WINAPI Main(HMODULE hModule)
 {
 	Memory mem;
+
+	mem.nop(signatures::initialized::moneyChange);
+
+	while (!GetAsyncKeyState(VK_END))
+	{
+		_Offset_Ptr<float> pStamina = mem.get_pointer<float>(offsets::initialized::actor::stamina);
+		_Offset_Ptr<float> pHP = mem.get_pointer<float>(offsets::initialized::actor::HP);
+		_Offset_Ptr<float> pWeight = mem.get_pointer<float>(offsets::initialized::actor::weight);
+		_Offset_Ptr<float> pCrosshairDelayInfo = mem.get_pointer<float>(offsets::initialized::actor::crosshairDelayInfo);
+		_Offset_Ptr<float> pFov = mem.get_pointer<float>(offsets::initialized::actor::fov);
+
+		_Offset_Ptr<float> pX = mem.get_pointer<float>(offsets::initialized::actor::coordX);
+		_Offset_Ptr<float> pY = mem.get_pointer<float>(offsets::initialized::actor::coordY);
+		_Offset_Ptr<float> pZ = mem.get_pointer<float>(offsets::initialized::actor::coordZ);
+
+		_Offset_Ptr<const char> pActorName = mem.get_pointer<const char>(offsets::initialized::actor::name);
+
+		_Offset_Ptr<size_t> pActorFireState = mem.get_pointer<size_t>(offsets::initialized::actor::fireState);
+		_Offset_Ptr<size_t> pTargetType = mem.get_pointer<size_t>(offsets::initialized::actor::targetType);
+		_Offset_Ptr<size_t> pPlayerState = mem.get_pointer<size_t>(offsets::initialized::actor::player_state);
+
+		_Offset_Ptr<int> pMoney = mem.get_pointer<int>(offsets::initialized::actor::money);
+
+		_Offset_Ptr<bool> pWeaponInHands = mem.get_pointer<bool>(offsets::initialized::actor::weaponIsInHands);
+
+		pStamina = 1;
+
+		pWeight = 0;
+
+		if (*pMoney < 1000000)
+			pMoney += 100000;
+
+		if (pWeaponInHands)
+			if (pTargetType == game::target_type::alive)
+				if (pActorFireState == game::fire_state::can_shoot)
+					if (GetForegroundWindowName() == "xrEngine.exe")
+					{
+						mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+						mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+					}
+
+		if (pPlayerState != game::player_state::flying && pPlayerState != 
+			game::player_state::flyingWithStaminaWaste && GetAsyncKeyState(VK_SPACE))
+		{
+			pPlayerState = 1;
+			pPlayerState = 256;
+			Sleep(50);
+			pPlayerState = 0;
+		}
+
+		pCrosshairDelayInfo = 1;
+	}
 
 	FreeLibraryAndExitThread(hModule, 0);
 }
