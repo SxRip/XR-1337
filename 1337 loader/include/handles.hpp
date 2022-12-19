28c3502b03311141c63e0a6b953ff0b80ea41ba3
Sora <68508786+SxRip@users.noreply.github.com>
2022-12-19 21:27:31 +0100

    updated handles file

--- 1337 loader/include/handles.hpp
+++ 1337 loader/include/handles.hpp
@@ -6,35 +6,72 @@ class handle_ptr
 {
 public:
 	handle_ptr() = default;
-	handle_ptr(HANDLE handle) : _Handle{ handle }
+	handle_ptr(HANDLE& handle) : _Handle{ &handle } {}
+
+	handle_ptr(HANDLE&& handle) : handle_ptr(handle)
 	{}
 
 	~handle_ptr()
 	{
-		if (_Handle)
-		{
-			_CloseHandle(_Handle);
-			_Handle = nullptr;
-		}
+		_VERIFY();
+	}
+	
+	inline handle_ptr& operator=(HANDLE& handle) noexcept
+	{
+		_VERIFY();
+
+		if (handle)
+			_Handle = &handle;
+		return *this;
 	}
 
-	inline handle_ptr& operator=(HANDLE handle) noexcept
+	inline handle_ptr& operator=(HANDLE&& handle) noexcept
 	{
-		_Handle = handle;
+		return this->operator=(handle);
+	}
+
+	inline handle_ptr& operator=(handle_ptr& ptr_handle) noexcept
+	{
+		_VERIFY();
+
+		if (ptr_handle._Handle)
+			_Handle = &*ptr_handle._Handle;
+
 		return *this;
+	}
+
+	inline handle_ptr& operator=(handle_ptr&& ptr_handle) noexcept
+	{
+		return this->operator=(ptr_handle);
 	}
 
 	inline operator bool() const noexcept
 	{
-		return _Handle != nullptr && _Handle != INVALID_HANDLE_VALUE;
+		return _Handle && *_Handle != INVALID_HANDLE_VALUE;
 	}
 
-	inline bool operator==(handle_ptr& _Right) noexcept { return _Handle == _Right.get(); }
+	inline bool operator==(handle_ptr& _Right) const noexcept { return *_Handle == *_Right._Handle; }
 
-	HANDLE& get() noexcept { return _Handle; }
+	HANDLE get() const noexcept { return _Handle ? *_Handle : nullptr; }
 
-	handle_ptr& close() noexcept { _CloseHandle(_Handle); _Handle = nullptr; return *this; }
+	handle_ptr& close() noexcept
+	{
+		_VERIFY();
+		return *this;
+	}
 
 private:
-	HANDLE _Handle;
+
+	void _VERIFY() noexcept
+	{
+		if (_Handle && *_Handle)
+		{
+			_CloseHandle(*_Handle);
+			*_Handle = nullptr;
+		}
+
+		_Handle = nullptr;
+	}
+
+	HANDLE* _Handle;
 };
